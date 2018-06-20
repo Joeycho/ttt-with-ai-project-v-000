9ed915ac1df24b48153f48221b128e9288f5a0aa
Joey Cho <cjy12182000@naver.com>
2018-06-20 13:31:37 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,7 +5,7 @@ module Players
     def move(board)
     #  binding.pry
     index = [0,1,2,3,4,5,6,7,8]
-    .each do
+    index.each do
     |a| if board.valid_move?(a.to_s)
         return a.to_s
     end
