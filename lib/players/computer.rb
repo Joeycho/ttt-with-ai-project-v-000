7f81c17ee4e911d33d104ec621c9147b36e5deb9
Joey Cho <cjy12182000@naver.com>
2018-06-20 14:10:22 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,16 +3,16 @@ module Players
     attr_accessor :input
 
     def move(board)
-    #  binding.pry
-  #  index = [0,1,2,3,4,5,6,7,8]
-  #  index.each do
-#    |a| if board.valid_move?(a.to_s)
+      binding.pry
+    index = [0,1,2,3,4,5,6,7,8]
+    index.each do
+    |a| if board.valid_move?(a.to_s)
       #  binding.pry
-#        return (a+1).to_s
+        return (a+1).to_s
 
-#        end
-#    end
-      "1"
+        end
+    end
+
     end
   end
 end
